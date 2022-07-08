32757fd5e73b1b723fcef39120c0cde59036be29
Anilrathod1738 <106953347+Anilrathod1738@users.noreply.github.com>
2022-07-08 17:02:52 +0530

    Add files via upload

---
+++ Substraction_matrix.java
@@ -0,0 +1,69 @@
+import java.util.*;
+class Substraction_matrix
+{
+ public static void main(String []args)
+ {
+   Scanner sc=new Scanner(System.in);
+    int row,col;
+ 
+ int[][] a=new int[3][3];
+ int[][] b=new int[3][3];
+ int[][] c=new int[3][3];
+
+ System.out.println("Enter first matrix elements");
+ 
+ for(row=0;row<3;row++)
+  {
+   for(col=0;col<3;col++)
+    
+      a[row][col]=sc.nextInt();
+      }
+
+
+ System.out.println("Enter second matrix elements");
+ 
+ for(row=0;row<3;row++)
+  {
+   for(col=0;col<3;col++)
+    
+      b[row][col]=sc.nextInt();
+      }
+      
+ 
+  for(row=0;row<3;row++)
+  {
+   for(col=0;col<3;col++)
+    
+      c[row][col]=a[row][col]-b[row][col];
+      }
+         System.out.println(" first matrix elements");
+ 
+        for(row=0;row<3;row++)
+         {
+          for(col=0;col<3;col++)
+    
+        System.out.print(a[row][col]+" ");
+        System.out.println();
+      }
+      System.out.println(" second matrix elements");
+ 
+        for(row=0;row<3;row++)
+         {
+          for(col=0;col<3;col++)
+    
+        System.out.print(b[row][col]+" ");
+        System.out.println();
+      }
+ 
+         System.out.println(" sum of  matrix elements");
+ 
+        for(row=0;row<3;row++)
+         {
+          for(col=0;col<3;col++)
+    
+        System.out.print(c[row][col]+" ");
+        System.out.println();
+      }
+   }
+}
+ 
